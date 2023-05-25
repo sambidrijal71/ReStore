d0fe07df56a3c7965334e2285b0a700c563facc7
srijal <srijal@planittesting.com>
2023-05-25 23:47:04 +1000

    Project setup & get product

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+
+
+  public class ProductsController : ControllerBase
+  {
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+      _context = context;
+
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProduct()
+    {
+      var products = await _context.Products.ToListAsync();
+      return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> getProduct(int id)
+    {
+      return await _context.Products.FindAsync(id);
+    }
+  }
+}
